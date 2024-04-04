fb9542d2d4f3e3d800da720f0a9caeddbe8c4076
Diana Palardy <108351091+dqpalardy@users.noreply.github.com>
2024-04-04 16:58:44 -0400

    Add files via upload

--- story_content/user.js
+++ story_content/user.js
@@ -2,38 +2,14 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6J4RoYXDfnu":
+      case "6UdQeDrM2JV":
         Script1();
         break;
-      case "6bHbuNgApsM":
+      case "6CbZnZzPAQN":
         Script2();
         break;
-      case "6UVIEnKmmoW":
+      case "5sH0wKFMzRn":
         Script3();
-        break;
-      case "6SPV7pymiRR":
-        Script4();
-        break;
-      case "6ONsXysMb74":
-        Script5();
-        break;
-      case "5ejrkwwLtlD":
-        Script6();
-        break;
-      case "6Bd6EEMVZ9Y":
-        Script7();
-        break;
-      case "5ctqohX7nqJ":
-        Script8();
-        break;
-      case "5yd2OItQPa2":
-        Script9();
-        break;
-      case "6LjxVaABDT3":
-        Script10();
-        break;
-      case "6qPtYeFnlg8":
-        Script11();
         break;
   }
 }
@@ -46,146 +22,6 @@ var addToTimeline = player.addToTimeline;
 var setVar = player.SetVar;
 var getVar = player.GetVar;
 window.Script1 = function()
-{
-  const target = object('6MOoRRCNzFW');
-const duration = 750;
-const easing = 'ease-out';
-const id = '6Dn8nyCmS1G';
-const growAmount = 0.2;
-player.addForTriggers(
-id,
-target.animate([
-{ scale: `${1 + growAmount}` }
-],
-  { fill: 'forwards', duration, easing }
-)
-);
-}
-
-window.Script2 = function()
-{
-  const target = object('6MOoRRCNzFW');
-const duration = 750;
-const easing = 'ease-out';
-const id = '6Dn8nyCmS1G_reverse';
-const growAmount = 0;
-player.addForTriggers(
-id,
-target.animate([
-{ scale: `${1 + growAmount}` }
-],
-  { fill: 'forwards', duration, easing }
-)
-);
-}
-
-window.Script3 = function()
-{
-  const target = object('6HXxzubKHet');
-const duration = 750;
-const easing = 'ease-out';
-const id = '6b5KY5dxMwz';
-const growAmount = 0.2;
-player.addForTriggers(
-id,
-target.animate([
-{ scale: `${1 + growAmount}` }
-],
-  { fill: 'forwards', duration, easing }
-)
-);
-}
-
-window.Script4 = function()
-{
-  const target = object('6HXxzubKHet');
-const duration = 750;
-const easing = 'ease-out';
-const id = '6b5KY5dxMwz_reverse';
-const growAmount = 0;
-player.addForTriggers(
-id,
-target.animate([
-{ scale: `${1 + growAmount}` }
-],
-  { fill: 'forwards', duration, easing }
-)
-);
-}
-
-window.Script5 = function()
-{
-  const target = object('6Spu2kPMjJX');
-const duration = 750;
-const easing = 'ease-out';
-const id = '6Yt290AqTZY';
-const growAmount = 0.2;
-player.addForTriggers(
-id,
-target.animate([
-{ scale: `${1 + growAmount}` }
-],
-  { fill: 'forwards', duration, easing }
-)
-);
-}
-
-window.Script6 = function()
-{
-  const target = object('6Spu2kPMjJX');
-const duration = 750;
-const easing = 'ease-out';
-const id = '6Yt290AqTZY_reverse';
-const growAmount = 0;
-player.addForTriggers(
-id,
-target.animate([
-{ scale: `${1 + growAmount}` }
-],
-  { fill: 'forwards', duration, easing }
-)
-);
-}
-
-window.Script7 = function()
-{
-  const target = object('6hEW01cECRf');
-const duration = 750;
-const easing = 'ease-out';
-const id = '5mjX2JBhKI8';
-const pulseAmount = 0.07;
-player.addForTriggers(
-id,
-target.animate([
-{ scale: '1' }, { scale: `${1 + pulseAmount}` },
-{ scale: '1' }, { scale: `${1 + pulseAmount}` },
-{ scale: '1' }
-],
-  { fill: 'forwards', duration, easing }
-)
-);
-}
-
-window.Script8 = function()
-{
-  const target = object('6hEW01cECRf');
-const duration = 750;
-const easing = 'ease-out';
-const id = '5mjX2JBhKI8_reverse';
-const pulseAmount = 0.07;
-player.addForTriggers(
-id,
-target.animate([
-{ scale: '1' }, { scale: `${1 + pulseAmount}` },
-{ scale: '1' }, { scale: `${1 + pulseAmount}` },
-{ scale: '1' }
-],
-  { fill: 'forwards', duration, easing }
-)
-);
-}
-
-window.Script9 = function()
 {
   player.once(() => {
 const target = object('6awtuyrsPW4');
@@ -204,7 +40,7 @@ target.animate([
 });
 }
 
-window.Script10 = function()
+window.Script2 = function()
 {
   player.once(() => {
 const target = object('5gSpJGxQ6fh');
@@ -223,7 +59,7 @@ target.animate([
 });
 }
 
-window.Script11 = function()
+window.Script3 = function()
 {
   player.once(() => {
 const target = object('5kaBw1T4FaD');
